b0f5c7b787a0ee527a1c1d4f8f2026aa688f72d5
juanBautista <juanbautista19@gmail.com>
2022-06-11 19:32:02 +0200

    Adding constructor calling the parent

--- src/main/java/deck/of/cards/freanchdeck/FrenchCard.java
+++ src/main/java/deck/of/cards/freanchdeck/FrenchCard.java
@@ -1,21 +1,18 @@
 package deck.of.cards.freanchdeck;
 
 import deck.of.cards.card.Card;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class FrenchCard extends Card {
 
     private FrenchFace face;
     private FrenchSuit suit;
+
+    public FrenchCard(FrenchFace face, FrenchSuit suit) {
+        super(face, suit);
+    }
 
 }
