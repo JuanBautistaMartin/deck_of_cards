db65a46e91ee2c4be4c2a55521cf56331f14394c
juanBautista <juanbautista19@gmail.com>
2022-06-11 19:12:44 +0200

    Creating new deckFactory

---
+++ src/main/java/deck/of/cards/DeckFactory.java
@@ -0,0 +1,36 @@
+package deck.of.cards;
+
+import deck.of.cards.card.Card;
+import deck.of.cards.deck.Deck;
+import deck.of.cards.freanchdeck.FrenchCard;
+import deck.of.cards.freanchdeck.FrenchFace;
+import deck.of.cards.freanchdeck.FrenchSuit;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Stream;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class DeckFactory {
+
+  public static Deck createDeck(DeckTypes type) {
+    switch (type) {
+      case FRENCH:
+        return createFrenchDeck();
+      default:
+        return new Deck();
+    }
+  }
+
+  private static Deck createFrenchDeck() {
+    Set<Card> cardList = new HashSet<>();
+    Stream.of(FrenchSuit.values())
+      .forEach(suit -> Stream.of(FrenchFace.values()).forEach(
+        face -> cardList.add(new FrenchCard(face, suit))
+      ));
+    return new Deck(cardList);
+  }
+
+}
