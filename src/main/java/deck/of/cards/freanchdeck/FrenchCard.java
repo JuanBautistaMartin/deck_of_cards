a5b2befad88e498fa3c349fe339dd464b67f467e
juanBautista <juanbautista19@gmail.com>
2022-06-11 19:12:29 +0200

    Adding constructors by lombok

--- src/main/java/deck/of/cards/freanchdeck/FrenchCard.java
+++ src/main/java/deck/of/cards/freanchdeck/FrenchCard.java
@@ -1,13 +1,18 @@
 package deck.of.cards.freanchdeck;
 
 import deck.of.cards.card.Card;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class FrenchCard extends Card {
 
     private FrenchFace face;
