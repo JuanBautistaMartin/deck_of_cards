83c21c6cdd6e4cd4cd97315db2762df7f735fb7e
juanBautista <juanbautista19@gmail.com>
2022-06-11 19:32:43 +0200

    Creating a frenchDeck and printing it as first thig done in Main class

--- src/main/java/deck/of/cards/Main.java
+++ src/main/java/deck/of/cards/Main.java
@@ -1,8 +1,16 @@
 package deck.of.cards;
 
+import deck.of.cards.deck.Deck;
+
 public class Main {
 
   public static void main(String[] args) {
+
+    Deck frenchDeck = DeckFactory.createDeck(DeckTypes.FRENCH);
+
+    // printing new created deck
+    System.out.println("French deck: ");
+    System.out.println(frenchDeck.toString());
 
   }
 
